cd3b23a6106628d87221ae3234cf684d9892c5e6
jkafka <Nejdelsiheslonasvete1>
2020-09-28 14:54:48 +0200

    new files

--- Luhn_test.py
+++ Luhn_test.py
@@ -1,10 +1,20 @@
-def Luhn_test(digits):
-    reverse_list = digits[::-1]
-    odd_list = []
-    for my_numer in reverse_list:
-        if not my_numer % 2 == 0:
-            odd_list.append(my_numer)
-        else: continue
+def luhn(num):
+    num_str = str(num).replace(' ', '')
+    s1 = s2 = 0
+    for i, num in enumerate(num_str[::-1]):
+        if i % 2 == 0:
+            s1 += int(num)
+        else:
+            num = int(num) * 2
+            if num > 9:
+                num = int(str(num)[0]) + int(str(num)[1])
+            s2 += int(num)
+    result = True if (s1 + s2) % 10 == 0 else False
+    return result
+
+num = '61789372994'
+print(luhn(num))
+
 
 
 
